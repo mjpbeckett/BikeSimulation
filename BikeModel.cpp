5cee9354c356e45869fa53873fe6f6862a8f4d10
Matt Beckett <matbecket@gmail.com>
2022-02-16 21:13:00 -0500

    Factor in change in elevation

--- BikeModel.cpp
+++ BikeModel.cpp
@@ -14,6 +14,7 @@ class rider {
         float pos_x, pos_y ; // x and y positions
         float vel ; // velocity along road
         float power ; // power output in watts
+        float gradient ; // expressed as ratio, not as percentage
         float time ;
 
         rider (std::string& ridername, float ridermass, float Crr_def = 0.004, float Cda_def = 0.4) {
@@ -21,8 +22,9 @@ class rider {
             mass_kg = ridermass ;
             Crr = Crr_def , CdA = Cda_def ;
             pos_x = 0 , pos_y = 0 ;
-            vel = 0 ; // set velocity to 1 to avoid issues of infinite force
+            vel = 0 ;
             power = 0 ;
+            gradient = 0 ;
             time = 0 ;
         }
 
@@ -35,13 +37,20 @@ class rider {
         }
 
         float lost_power_to_rolling() {
-            return grav*mass_kg*Crr*vel ;
+            float angle_correction = 1 / (1 + gradient*gradient) ; // equal to cos(arctan(gradient))
+            return grav*mass_kg*Crr*vel*angle_correction ;
+        }
+
+        float lost_power_to_gravity() {
+            float angle_factor = gradient / (1 + gradient*gradient) ; // equal to sin(arctan(gradient))
+            return grav*mass_kg*vel*angle_factor ;
         }
 
         void update(float time_step=1) {
-            float power_added = power_at_wheel() - lost_power_to_drag() - lost_power_to_rolling() ;
+            float power_added = power_at_wheel() - lost_power_to_drag() - lost_power_to_rolling() - lost_power_to_gravity();
             time += time_step ;
-            pos_x += vel*time_step ;
+            pos_x += vel*time_step*(1/(1+gradient*gradient)) ;
+            pos_y += vel*time_step*(gradient/(1+gradient*gradient)) ;
             vel = sqrt(vel*vel + 2*(power_added/mass_kg)) ;
         }
 } ;
@@ -51,10 +60,14 @@ int main () {
     // std::string name = "Me" ;
     // rider myrider (name, 75);
     // myrider.power = 200 ;
-    // for (int i = 0; i < 200; i++) {
-    //     std::cout << myrider.vel << std::endl ;
-    //     myrider.update() ;
+    // myrider.gradient = 0.085 ;
+    // while (myrider.pos_y < 1046) {
+    //     myrider.update();
     // }
+    // std::cout << "Time: " ;
+    // std::cout << (myrider.time)/60 << std::endl ;
+    // std::cout << "Speed: " ;
+    // std::cout << (myrider.vel)*3.6 << std::endl ;
 
     return 0 ;
 }
